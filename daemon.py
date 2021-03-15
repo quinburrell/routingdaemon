4972fb74ac8917eb1f5f38913a1dc65d6d89310e
quin <burellq@gmail.com>
2021-03-16 11:15:54 +1300

    skeleton

--- daemon.py
+++ daemon.py
@@ -1,2 +1,10 @@
-#this is some code
-#and so is this
+#TODO
+#read config function
+#open connection function
+def hello(port):
+    """constructs a hello packet and sends it to port"""
+def readconfig(filename):
+    """reads the configfile inputed by the user
+    assigns itself a router ID, inpu and output ports and starts a timer on itself"""
+def openports(ports):
+    """opens UDP connections with ports"""
