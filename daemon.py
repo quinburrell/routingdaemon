6f3e56279cdfee0dc5ceb288ac2ba38db5c7a6b2
quinburrell <79877697+quinburrell@users.noreply.github.com>
2021-05-06 11:31:14 +1200

    Delete daemon.py
    Needless

--- daemon.py
+++
@@ -1,9 +0,0 @@
-#TODO
-
-def hello(port):
-    """constructs a hello packet and sends it to port"""
-def readconfig(filename):
-    """reads the configfile inputed by the user
-    assigns itself a router ID, inpu and output ports and starts a timer on itself"""
-def openports(ports):
-    """opens UDP connections with ports"""
